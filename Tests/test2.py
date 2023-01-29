44f9755fe7d7dfe521e8b3df73fb0dd108bf6c7e
DT3MP01 <albertoalvaza@gmail.com>
2023-01-29 13:01:03 +0100

    Update test2.py

--- Tests/test2.py
+++ Tests/test2.py
@@ -1,8 +1,10 @@
 # Find square root of real or complex numbers
 # Importing the complex math module
 import cmath
+import sys
 
-num = 1+2j
+myArgs = sys.argv[1:]
+num = int(myArgs[0])
 
 # To take input from the user
 #num = eval(input('Enter a number: '))
